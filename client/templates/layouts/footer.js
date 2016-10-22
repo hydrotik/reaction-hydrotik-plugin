904023c63360f92c47643f5fc9d9cae6faaeaeda
Donovan Adams <donovan@hydrotik.com>
2016-10-22 19:30:28 -0400

    started hooking footer into shop dynamic settings

--- client/templates/layouts/footer.js
+++ client/templates/layouts/footer.js
@@ -1,7 +1,48 @@
 import { Reaction } from "/client/api";
-import { Media, Shops } from "/lib/collections";
+import { Media, Shops, Packages } from "/lib/collections";
+import { merge } from "lodash";
+
+Template.layoutFooterHydrotik.onCreated(function () {
+  this.settings = ReactiveVar();
+
+  // Update product subscription
+  this.autorun(() => {
+
+
+    const shop = Shops.findOne(Reaction.getShopId());
+    const addressBook = shop.addressBook[0];
+    const email = shop.emails[0].address;
+
+    console.log(shop);
+
+    const footerSettings = {
+        address1: addressBook.address1,
+        address2: addressBook.address2,
+        city: addressBook.city,
+        country: addressBook.country,
+        fullName: addressBook.fullName,
+        phone: addressBook.phone,
+        postal: addressBook.postal,
+        region: addressBook.region,
+        email: email,
+        description: shop.description,
+        name: shop.description,
+        year: new Date().getFullYear()
+    };
+    console.log(footerSettings);
+
+    this.settings.set(footerSettings);
+  });
+});
 
 Template.layoutFooterHydrotik.helpers({
+  settings: function () {
+    //const template = Template.instance();
+    //return template && template.socialSettings;
+
+    
+    return Template.instance().settings.get();
+  },
   logo() {
     const shop = Shops.findOne(Reaction.getShopId());
 
