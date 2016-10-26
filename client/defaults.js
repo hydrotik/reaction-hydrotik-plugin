6ddfcf68fd0f3083f70df3adf0177ede83188a42
Donovan Adams <donovan@hydrotik.com>
2016-10-25 22:11:58 -0400

    rename index to home and also fixe notFound value

--- client/defaults.js
+++ client/defaults.js
@@ -2,14 +2,12 @@ import { Session } from "meteor/session";
 import { Logger } from "/client/api";
 
 Session.set("DEFAULT_LAYOUT", "coreLayoutHydrotik");
-Logger.info("setting DEFAULT_LAYOUT");
-
 
 Session.set("INDEX_OPTIONS", {
-  template: "indexLayoutHydrotik",
+  template: "homeLayoutHydrotik",
   layoutHeader: "layoutHeaderHydrotik",
   layoutFooter: "layoutFooterHydrotik",
-  notFound: "notFound",
+  notFound: "productNotFound",
   dashboardControls: "dashboardControls",
   adminControlsFooter: "adminControlsFooter"
 });
